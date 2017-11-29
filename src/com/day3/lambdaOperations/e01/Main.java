f5021e3fb5ecc3ee199ebcfc4c8a4902c8e21b58
polosate <polosate@gmail.com>
2017-11-29 11:20:08 +0300

    Day 3. Lambda operations, part 1

---
+++ src/com/day3/lambdaOperations/e01/Main.java
@@ -0,0 +1,15 @@
+package com.day3.lambdaOperations.e01;
+
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      MapTest.main(args);
+      PeekTest.main(args);
+      
+  }
+}
